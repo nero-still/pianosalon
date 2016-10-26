0143b7fa96a1fc91263be13d735a6d7bfc72b2eb
Rainer Mühr <rainer.muehr@framefield.com>
2016-10-26 23:52:20 +0200

    removed default structure

--- routes.js
+++ routes.js
@@ -1,6 +1,23 @@
-Router.route('/', {name:'homepage'});
+// Router.route('/', {name:'homepage'});
 
-// Router.configure({
-//     layoutTemplate:   "LayoutWithHeader",
-//     notFoundTemplate: "notFound"
-// });
+Router.configure({
+    layoutTemplate:   "LayoutWithHeader",
+    // notFoundTemplate: "notFound"
+});
+
+Router.onBeforeAction(function() {
+    if (Meteor.user()) {
+        this.next();
+    }
+    else {
+        Router.go('homepage');
+    }},
+    {except: ['info.page']
+});
+
+Router.route('/', function () {
+  this.render('homepage', {
+    data: function () { return Items.findOne({_id: this.params._id}); }
+  });
+});
+       
